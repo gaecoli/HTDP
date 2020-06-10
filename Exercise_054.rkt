4dd312e59f307d5e0e7658a1abed2da590d9d581
peterlee0304 <peterlee2016@163.com>
2020-06-10 23:35:15 +0800

    solutions

--- Exercise_054.rkt
+++ Exercise_054.rkt
@@ -1,36 +1,7 @@
 #lang racket
-(require 2htdp/batch-io)
 
-(define (letter fst lst signature-name)
-  (string-append
-    (opening fst)
-    "\n\n"
-    (body fst lst)
-    "\n\n"
-    (closing signature-name)))
- 
-(define (opening fst)
-  (string-append "Dear " fst ","))
- 
-(define (body fst lst)
-  (string-append
-   "We have discovered that all people with the" "\n"
-   "last name " lst " have won our lottery. So, " "\n"
-   fst ", " "hurry and pick up your prize."))
- 
-(define (closing signature-name)
-  (string-append
-   "Sincerely,"
-   "\n\n"
-   signature-name
-   "\n"))
-
-(write-file "in-fst" "Matthew")
-(write-file "in-lst" "Fisler")
-(write-file "in-signature" "Felleisen")
-
-(define (main in-fst in-lst in-signature out)
-  (write-file out
-              (letter (read-file in-fst)
-                      (read-file in-lst)
-                      (read-file in-signature))))
+(define (show x)
+  (cond
+    [(= x 0) ...]
+    [(<= -3 x -1) ...]
+    [(>= x 0) ...]))
--- Exercise_055.rkt
+++ Exercise_055.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket 
 
 (define (show x)
   (cond

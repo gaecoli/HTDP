0df1e35d0b86e2eb43cf96a60281e61ed0635891
peterlee0304 <peterlee2016@163.com>
2020-06-19 23:30:27 +0800

    solutions

---
+++ Exercise_075.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(define-struct vel [deltax deltay])
+(define-struct ufo [loc vel])
+
+(define v1 (make-vel 8 -3))
+(define v2 (make-vel -5 -3))
+
+(define p1 (make-posn 22 80))
+(define p2 (make-posn 30 77))
+
+(define u1 (make-ufo p1 v1))
+(define u2 (make-ufo p1 v2))
+(define u3 (make-ufo p2 v1))
+(define u4 (make-ufo p2 v2))
+
+(check-expect (ufo-move-1 u1) u3)
+(check-expect (ufo-move-1 u2) (make-ufo (make-posn 17 77) v2))
+(define (ufo-move-1 u)
+  (make-ufo (posn+ (ufo-loc u) (ufo-vel u)) (ufo-vel u)))
+
+(check-expect (posn+ p1 v1) p2)
+(check-expect (posn+ p1 v2) (make-posn 17 77))
+(define (posn+ p v)
+  (make-posn (+ (posn-x p) (vel-deltax v))
+             (+ (posn-y p) (vel-deltay v))))
---
+++ Exercise_076.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(define-struct movie [title producer year])
+(define-struct person [name hair eyes phone])
+(define-struct pet [name number])
+(define-struct CD [artist title price])
+(define-struct sweater [material size producer])
